fcc0b7b6239b8980ebe435462956d430a8a4d607
Robert Fisher <fisher@tum.de>
2019-04-17 22:15:36 +1200

    Add some menu stuff

--- frontend/src/components/menu/Menu.jsx
+++ frontend/src/components/menu/Menu.jsx
@@ -2,34 +2,58 @@ import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import './Menu.scss'
 
-const PageLink = withRouter(({ to, text, icon, location }) => {
-  const active = location.pathname === to
+const LinkWithIcon = withRouter(({ to, text, icon, location, matches, className }) => {
+  const active = location.pathname === to || (matches && matches.some(m => m === location.pathname))
   return (
     <Link
       to={to}
-      className={'plain' + (active ? ' active' : '')}
+      className={'plain' + (active ? ' active' : '') + (className ? ' ' + className : '')}
       onClick={e => {
         if (active) e.preventDefault()
       }}
     >
-      <div className="icon-container">
-        <i className={icon} />
-      </div>
-      {text}
+      <div className="icon-container">{typeof icon === 'string' ? <i className={icon} /> : icon}</div>
+      <span>{text}</span>
     </Link>
   )
 })
 
-const Menu = () => (
-  <div className="menu">
-    <div className="page-selection">
-      <PageLink to="/" text="NROs" icon="nro" />
-      <PageLink to="/program" text="Program" icon="programme" />
-      <PageLink to="/projects" text="Projects" icon="project" />
+const Menu = ({ location }) => {
+  const nroPage = location.pathname === '/' || location.pathname === '/breakdowns'
+  return (
+    <div className="menu">
+      <div className="page-selection">
+        <LinkWithIcon to="/" text="NROs" icon="nro" matches={['/breakdowns']} />
+        <LinkWithIcon to="/program" text="Program" icon="programme" />
+        <LinkWithIcon to="/projects" text="Projects" icon="project" />
+      </div>
+      {nroPage && (
+        <React.Fragment>
+          <div className="view-selection">
+            <div className="title">View</div>
+            <div className="view-switch">
+              <LinkWithIcon to="/" text="Overview" icon="map" className="overview" />
+              <LinkWithIcon
+                to="/breakdowns"
+                text="Breakdowns"
+                icon={
+                  <div>
+                    <span />
+                    <span />
+                    <span />
+                  </div>
+                }
+                className="breakdowns"
+              />
+            </div>
+          </div>
+          <div className="data-selection">
+            <div className="title">Data to show</div>
+          </div>
+        </React.Fragment>
+      )}
     </div>
-    <div className="view-selection" />
-    <div className="data-selection" />
-  </div>
-)
+  )
+}
 
-export default Menu
+export default withRouter(Menu)
