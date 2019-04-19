8739d5a96927b68ec3fde99122d0fa8e746cd5d1
Robert Fisher <fisher@tum.de>
2019-04-20 10:36:03 +1200

    Scroll styling

--- frontend/src/components/pages/NroPage.jsx
+++ frontend/src/components/pages/NroPage.jsx
@@ -9,14 +9,16 @@ const NroPage = ({ location }) => {
   const dataSelectionCapitalized = dataSelection.charAt(0).toUpperCase() + dataSelection.slice(1)
   const breakdowns = location.pathname === '/breakdowns'
   return (
-    <div className="nro-page">
-      <h1>
-        <span className="light">Showing </span>
-        <span>{dataSelectionCapitalized + ' per NRO'}</span>
-        <span className="light">{breakdowns ? ' with ' : ' at a '}</span>
-        <span>{breakdowns ? 'breakdowns' : 'relative scale'}</span>
-      </h1>
-      {breakdowns ? <PieGrid /> : <Map />}
+    <div className="vertical-scroll-container">
+      <div className="nro-page">
+        <h1>
+          <span className="light">Showing </span>
+          <span>{dataSelectionCapitalized + ' per NRO'}</span>
+          <span className="light">{breakdowns ? ' with ' : ' at a '}</span>
+          <span>{breakdowns ? 'breakdowns' : 'relative scale'}</span>
+        </h1>
+        {breakdowns ? <PieGrid /> : <Map />}
+      </div>
     </div>
   )
 }
