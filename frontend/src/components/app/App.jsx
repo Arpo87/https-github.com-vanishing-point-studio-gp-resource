08f98940a4994e8c51ff8dd10e8fbf95849352ab
Robert Fisher <fisher@tum.de>
2019-04-25 20:46:52 +1200

    Add apollo client

--- frontend/src/components/app/App.jsx
+++ frontend/src/components/app/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import WebFont from 'webfontloader'
+import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import BrowserCheck from '../browser/BrowserCheck'
 import Frame from '../frame/Frame'
@@ -12,14 +14,21 @@ WebFont.load({
   },
 })
 
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:4000' }),
+  cache: new InMemoryCache(),
+})
+
 const App = () => (
   <BrowserCheck>
-    <Router>
-      <Switch>
-        <Route path="/login" exact component={Login} />
-        <Route path="/" component={Frame} />
-      </Switch>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route path="/login" exact component={Login} />
+          <Route path="/" component={Frame} />
+        </Switch>
+      </Router>
+    </ApolloProvider>
   </BrowserCheck>
 )
 
--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,40 +1,57 @@
 import React from 'react'
+import { Mutation } from 'react-apollo'
+import { gql } from 'apollo-boost'
 import './Login.scss'
+
+const loginMutation = gql`
+  mutation loginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`
 
 class Login extends React.Component {
   state = { email: '', password: '' }
 
   render() {
     return (
-      <div className="login-page">
-        <form noValidate onSubmit={this.handleSubmit}>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            spellCheck={false}
-            value={this.state.email}
-            onChange={this.handleInputChange}
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            spellCheck={false}
-            value={this.state.password}
-            onChange={this.handleInputChange}
-          />
-          <button type="submit">Sign in</button>
-        </form>
-      </div>
+      <Mutation mutation={loginMutation} variables={this.state}>
+        {(signup, { loading }) => (
+          <div className="login-page">
+            <form
+              noValidate
+              onSubmit={async e => {
+                e.preventDefault()
+                await signup()
+                this.setState({ email: '', password: '' })
+              }}
+            >
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                spellCheck={false}
+                value={this.state.email}
+                onChange={this.handleInputChange}
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                spellCheck={false}
+                value={this.state.password}
+                onChange={this.handleInputChange}
+              />
+              <button type="submit">{loading ? 'Signing in...' : 'Sign in'}</button>
+            </form>
+          </div>
+        )}
+      </Mutation>
     )
   }
 
   handleInputChange = e => this.setState({ [e.target.name]: e.target.value })
-
-  handleSubmit = e => {
-    e.preventDefault()
-  }
 }
 
 export default Login
