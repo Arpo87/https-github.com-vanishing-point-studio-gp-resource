9349e460bd9a9fbb69a48e31b5d82a72e670ace0
Allan Walker <allan@numeralstudio.com>
2019-04-28 13:23:50 +1200

    Updated programme data.

--- backend/prisma/seeds/users.ts
+++ backend/prisma/seeds/users.ts
@@ -2,13 +2,25 @@ export const users = [
   {
     name: 'Allan Walker',
     email: 'allan@vanishingpoint.studio',
-    password: 'password',
+    password: 'One2bucklemyshoe',
     isAdmin: true,
   },
   {
     name: 'Rob Fisher',
     email: 'mail@robf.co.nz',
     password: 'password',
+    isAdmin: true,
+  },
+  {
+    name: 'Sarah Duthie',
+    email: 'sarah.duthie@greenpeace.org',
+    password: 'StayHe@lthy!',
+    isAdmin: true,
+  },
+  {
+    name: 'Guest',
+    email: 'sarah.duthie+guest@greenpeace.org',
+    password: 'TrueBlue22',
     isAdmin: true,
   },
 ]
