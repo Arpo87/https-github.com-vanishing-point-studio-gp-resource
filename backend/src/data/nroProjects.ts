9ef9fb9c0430ba49a4e546cf0e3c8c3474ec3504
Allan Walker <allan@numeralstudio.com>
2019-05-03 18:08:34 +1200

    Updated Projects for breakdowns, added missing NROs.

--- backend/src/data/nroProjects.ts
+++ backend/src/data/nroProjects.ts
@@ -1,8 +1,17 @@
 export const nroProjects = [
   {
+    meta: 'Africa Climate Justice',
     project: 1,
     nro: 1,
-    staff: 1,
-    budget: 1,
+    staffTotal: 0,
+    budgetTotal: 0,
+    additionalInfo: 'Info',
+    staffCampaignCoordination: 0,
+    staffCampaigns: 0,
+    staffEngagement: 0,
+    staffMediaComms: 0,
+    staffOperations: 0,
+    staffPolitical: 0,
+    staffPublicInfoOutreach: 0,
   }
 ]
