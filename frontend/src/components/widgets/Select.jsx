8be13f45912dfd6ab72e16fd8b4aa10311cbe5e1
Robert Fisher <fisher@tum.de>
2019-05-04 22:23:36 +1200

    Select styling

--- frontend/src/components/widgets/Select.jsx
+++ frontend/src/components/widgets/Select.jsx
@@ -6,7 +6,6 @@ class Select extends React.Component {
   state = {
     open: false,
     selection: this.props.options.length > 0 ? this.props.options[0] : '',
-    keyFocus: -1,
   }
 
   componentDidMount() {
@@ -23,19 +22,19 @@ class Select extends React.Component {
 
   render() {
     const { options } = this.props
-    const { open, selection, keyFocus } = this.state
+    const { open, selection } = this.state
+    const sortedOptions = [...options]
+    sortedOptions.sort((first, second) => (first === selection ? -1 : second === selection ? 1 : 0))
     return (
       <div className={'select-wrapper' + (open ? ' open' : '')}>
         {open && (
           <div className="anchor">
             <ul className="select-popup">
-              {options.map((option, i) => (
+              {sortedOptions.map((option, i) => (
                 <li
                   key={option}
                   role="button"
-                  className={
-                    'select-item' + (option === selection ? ' selected' : '') + (i === keyFocus ? ' key-focus' : '')
-                  }
+                  className={'select-item' + (option === selection ? ' selected' : '')}
                   onClick={() => this.updateSelection(option)}
                 >
                   {option}
@@ -44,7 +43,7 @@ class Select extends React.Component {
             </ul>
           </div>
         )}
-        <button type="button" ref={e => (this.buttonElement = e)} onKeyDown={this.handleButtonKeyDown}>
+        <button type="button" ref={e => (this.buttonElement = e)}>
           <span>{selection}</span>
         </button>
         <ExpandMoreIcon />
@@ -80,31 +79,6 @@ class Select extends React.Component {
 
   updateSelection = selection => {
     this.setState({ selection })
-  }
-
-  handleButtonKeyDown = e => {
-    if (e.key === 'ArrowDown' || e.keyCode === 40) {
-      this.incrementKeyFocus(1)
-    } else if (e.key === 'ArrowUp' || e.keyCode === 38) {
-      this.incrementKeyFocus(-1)
-    } else if (e.key === 'Enter' || e.keyCode === 13) {
-      if (this.state.keyFocus > -1) {
-        this.setState({ selection: this.props.options[this.state.keyFocus], open: false, keyFocus: -1 })
-        e.preventDefault()
-      }
-    }
-  }
-
-  incrementKeyFocus = amount => {
-    const l = this.props.options.length
-    let keyFocus
-    if (this.state.keyFocus === -1) {
-      keyFocus = amount > 0 ? 0 : l - 1
-    } else {
-      const sum = this.state.keyFocus + amount
-      keyFocus = sum >= 0 ? sum % l : (sum + l) % l
-    }
-    this.setState({ keyFocus })
   }
 }
 
