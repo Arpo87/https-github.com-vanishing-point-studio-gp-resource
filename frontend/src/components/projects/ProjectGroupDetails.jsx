2bcd93bc215d79ab8f7d8c133e64d751fc7a99dd
Robert Fisher <fisher@tum.de>
2019-05-06 22:07:07 +1200

    Allow relative bar widths in project group details

--- frontend/src/components/projects/ProjectGroupDetails.jsx
+++ frontend/src/components/projects/ProjectGroupDetails.jsx
@@ -4,15 +4,16 @@ import { formatValue } from '../../utils'
 import Breadcrumb from './Breadcrumb'
 import BarStack from '../bars/BarStack'
 import ProjectsLegend from './ProjectsLegend'
-
 import { getProjectGroupData } from '../../state/selectors'
 import './ProjectGroupDetails.scss'
 
+const relativeWidths = false
+
 const ProjectGroupDetails = ({ projects }) => {
+  const max = Math.max(...projects.map(p => p.totalBudget))
   return (
     <div className="project-group-details">
       <Breadcrumb name={projects[0].projectGroup} />
-
       <div className="legend-row">
         <div className="column" />
         <div className="column">
@@ -20,19 +21,24 @@ const ProjectGroupDetails = ({ projects }) => {
           <ProjectsLegend labels={projects[0].labels} />
         </div>
       </div>
-      {projects.map(project => (
-        <div className="bar-row" key={project.nro}>
-          <div className="column">
-            <div className="bar-title">
-              <div className="name">{project.nro}</div>
-              <div className="total">{formatValue(project.totalBudget, 'euro')}</div>
+      {projects.map(project => {
+        const width = relativeWidths ? (project.totalBudget / max) * 100 + '%' : '100%'
+        return (
+          <div className="bar-row" key={project.nro}>
+            <div className="column">
+              <div className="bar-title">
+                <div className="name">{project.nro}</div>
+                <div className="total">{formatValue(project.totalBudget, 'euro')}</div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="bar-stack-container" style={{ width }}>
+                <BarStack data={project} />
+              </div>
             </div>
           </div>
-          <div className="column">
-            <BarStack data={project} />
-          </div>
-        </div>
-      ))}
+        )
+      })}
     </div>
   )
 }
