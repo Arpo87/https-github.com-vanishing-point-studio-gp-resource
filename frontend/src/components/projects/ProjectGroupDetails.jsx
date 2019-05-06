668edfc878063594cb1531d1e720ddfc1c2411b4
Allan Walker <allan@numeralstudio.com>
2019-05-06 18:30:15 +1200

    Added bars to projects page.

--- frontend/src/components/projects/ProjectGroupDetails.jsx
+++ frontend/src/components/projects/ProjectGroupDetails.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Breadcrumb from './Breadcrumb'
+import BarStack from '../bars/BarStack'
 import './ProjectGroupDetails.scss'
 
+const data = {
+  total: 180,
+  values: [ 20, 50, 40, 70 ],
+  labels: [ 20, 50, 40, 70 ],
+}
 const ProjectGroupDetails = () => {
   return (
     <div className="project-group-details">
       <Breadcrumb name="Group" />
+      {/* Add <Legend /> copy BarsViewLegend */}
+      <BarStack data={data}/>
     </div>
   )
 }
